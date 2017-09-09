dd96d5082fdce9ffbffb17afc9ad12346fd2a99f
TamilMaran <30867933+Tamil-maran@users.noreply.github.com>
2017-09-09 07:23:09 +0530

    Update questionnaire_form.php
    Added Advanced Machine learning algorithms

--- views/questionnaire_form.php
+++ views/questionnaire_form.php
@@ -1,31 +1,21 @@
-	<div id="d2">
-	<h4 id="quest"> q00 </h4>
+<div id="d2" style="height:400px">
+	<h2 id="quest" style="height=300px; width="2000px"> Are you interested in designing a template or a visually appealing system ? </h2>
 		<li>
 			<input name="q1"  checked type="radio"></input>
-			<label id="lb1">Opt 1</label>
+			<label id="lb1" class="la" >Yes</label>
 			<div class="bullet">
 			</div>
 		</li>
 		<li>
 			<input name="q1" type="radio">
-			<label id="lb2">Opt 2</label>
+			<label id="lb2" class="la" >No</label>
 			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q1" type="radio">
-			<label id="lb3">Opt 3</label>
-			<div  class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q1" type="radio">
-			<label id="lb4">Opt 4</label>
-			<div  class="bullet">
 			</div>
 		</li>
 	</ul>
-	<br><br>
 	</div>
 
 	<button id="but" class="btn" onclick="nextques();">Next Question</button>
+	<input type="submit" id="sub" class=btn value="Submit" style="visibility: hidden">
+	<h4 id="s3" style="visibility: hidden; margin-left: 40px">The course best suited for you is</h4>
+	<button id="s1"  style="visibility: hidden; background-color: darkblue; height:50px; width:90px; margin-left: 50px; color:white">0</button>
