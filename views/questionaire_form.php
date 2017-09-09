a1d2072819153db12ff04997846bafd34e632faa
TamilMaran <30867933+Tamil-maran@users.noreply.github.com>
2017-09-09 05:32:45 +0530

    Update questionaire_form.php

--- views/questionaire_form.php
+++ views/questionaire_form.php
@@ -1,230 +1,61 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Find your Interests</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../public/css/question_style.css">
+</head>
+<body>
+<script src="../public/js/quest.js" ></script>
 
-	<h4 id="quest">Question 1</h4>
-	<ul>
+<header style="background:url('images/blue.jpeg')" style="padding:6em">
+  <h1>
+                Erogito
+  </h1>
+  <span>
+               A roadmap for CS
+  </span>
+</header>
+	<div id="d2">
+	<h4 id="quest"> q00 </h4>
 		<li>
-			<input name="q1"  type="radio" name="1">
-			<label>Answer 1</label>
+			<input name="q1"  checked type="radio"></input>
+			<label id="lb1">Opt 1</label>
 			<div class="bullet">
 			</div>
 		</li>
 		<li>
-			<input name="q1" type="radio" name="1">
-			<label>Answer 2</label>
+			<input name="q1" type="radio">
+			<label id="lb2">Opt 2</label>
 			<div class="bullet">
 			</div>
 		</li>
 		<li>
-			<input name="q1" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q2"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
+			<input name="q1" type="radio">
+			<label id="lb3">Opt 3</label>
+			<div  class="bullet">
 			</div>
 		</li>
 		<li>
-			<input name="q2" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q2" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
+			<input name="q1" type="radio">
+			<label id="lb4">Opt 4</label>
+			<div  class="bullet">
 			</div>
 		</li>
 	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q3"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q3" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q3" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q4"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q4" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q4" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q5"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q5" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q5" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q6"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q6" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q6" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q7"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q7" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q7" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q8"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q8" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q8" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q9"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q9" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q9" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-</div>
-  <div>
-	<h4 id="quest">Question 1</h4>
-	<ul>
-		<li>
-			<input name="q10"  type="radio" name="1">
-			<label>Answer 1</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q10" type="radio" name="1">
-			<label>Answer 2</label>
-			<div class="bullet">
-			</div>
-		</li>
-		<li>
-			<input name="q10" type="radio" name="1">
-			<label>Answer 3</label>
-			<div class="bullet">
-			</div>
-		</li>
-	</ul>
-  
+	<br><br>
+	</div>
+
+	<button id="but" class="btn" onclick="nextques();">Next Question</button>
+
+
+
+ <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+<footer>
+  <span style="padding:2em">Erogito(C) All rights Reserved</span>
+</footer>
+</body>
+</head>  
+</body>
+</html>
