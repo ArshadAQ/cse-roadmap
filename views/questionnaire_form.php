07f7462229af0020b35874862d8456ce094cb0f1
Arshad <arshadunderscotch@gmail.com>
2017-09-09 06:16:29 +0530

    questionnaire update

---
+++ views/questionnaire_form.php
@@ -0,0 +1,31 @@
+	<div id="d2">
+	<h4 id="quest"> q00 </h4>
+		<li>
+			<input name="q1"  checked type="radio"></input>
+			<label id="lb1">Opt 1</label>
+			<div class="bullet">
+			</div>
+		</li>
+		<li>
+			<input name="q1" type="radio">
+			<label id="lb2">Opt 2</label>
+			<div class="bullet">
+			</div>
+		</li>
+		<li>
+			<input name="q1" type="radio">
+			<label id="lb3">Opt 3</label>
+			<div  class="bullet">
+			</div>
+		</li>
+		<li>
+			<input name="q1" type="radio">
+			<label id="lb4">Opt 4</label>
+			<div  class="bullet">
+			</div>
+		</li>
+	</ul>
+	<br><br>
+	</div>
+
+	<button id="but" class="btn" onclick="nextques();">Next Question</button>
